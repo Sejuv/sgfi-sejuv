a98866a90559e9b8f6b5cb1fbe442db4058d9125
Taylan Itallo <taylan.itallo@gmail.com>
2026-01-27 02:16:39 -0300

    corrigir vite config e adicionar versão Node

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(projectRoot, 'src')
+      '@': path.resolve(__dirname, './src')
     }
   },
 });
